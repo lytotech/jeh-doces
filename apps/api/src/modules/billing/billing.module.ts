c2dae1324fd190d8af8e84b9f74164e596e49309
tallyto <rodrigues.tallyto@hotmail.com>
2026-09-04 20:33:12 -0300

    Corrige inicializacao do modulo de cobranca

--- apps/api/src/modules/billing/billing.module.ts
+++ apps/api/src/modules/billing/billing.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { BillingController } from './billing.controller';
 import { BillingService } from './billing.service';
+import { AuthModule } from '../auth/auth.module';
 
-@Module({ controllers: [BillingController], providers: [BillingService] })
+@Module({ imports: [AuthModule], controllers: [BillingController], providers: [BillingService] })
 export class BillingModule {}
